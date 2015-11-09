2fdb1bcfbc8aca8f35f5556b652fafd753b88d65
YasutakaAwano <eewano@gmail.com>
2015-11-09 20:14:27 +0900

    仮ステージを作成。 フォグ効果にて無限に道が続く様に設定。

---
+++ eewano_SampleGame01/Assets/Scripts/AreaGenerator.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AreaGenerator : MonoBehaviour {
+
+	const int AreaTipSize = 50;
+
+	int currentTipIndex;
+
+	//ターゲットキャラクターの指定
+	public Transform character;
+	//エリアチッププレファブ配列
+	public GameObject[] areaTips;
+	//自動生成開始インデックス
+	public int startTipIndex;
+	//生成先読み個数
+	public int preInstantiate;
+	//生成済みエリアチップ保持リスト
+	public List<GameObject> generatedAreaList = new List<GameObject>();
+
+	void Start()
+	{
+		currentTipIndex = startTipIndex - 1;
+		UpdateArea (preInstantiate);
+	}
+
+	void Update()
+	{
+		//キャラクターの位置から現在のエリアチップのインデックスを計算
+		int charaPositionIndex = (int)(character.position.z / AreaTipSize);
+
+		//次のエリアチップに入ったらエリアの更新処理を行なう
+		if (charaPositionIndex + preInstantiate > currentTipIndex) {
+			UpdateArea (charaPositionIndex + preInstantiate);
+		}
+	}
+
+	//指定のIndexまでのエリアチップを生成して、管理下に置く
+	void UpdateArea(int toTipIndex)
+	{
+		if (toTipIndex <= currentTipIndex)
+			return;
+
+		//指定のエリアチップまでを作成
+		for (int i = currentTipIndex + 1; i <= toTipIndex; i++) {
+			GameObject areaObject = GenerateArea (i);
+
+			//生成したエリアチップを管理リストに追加する
+			generatedAreaList.Add (areaObject);
+		}
+
+		//エリア保持上限内になるまで古いエリアを削除
+		while (generatedAreaList.Count > preInstantiate + 2)
+			DestroyOldestArea ();
+
+		currentTipIndex = toTipIndex;
+	}
+
+	//指定のインデックス位置にAreaオブジェクトをランダムに生成
+	GameObject GenerateArea(int tipIndex)
+	{
+		int nextAreaTip = Random.Range (0, areaTips.Length);
+
+		GameObject areaObject = (GameObject)Instantiate (
+			                        areaTips [nextAreaTip],
+			                        new Vector3 (0, 0, tipIndex * AreaTipSize),
+			                        Quaternion.identity
+		                        );
+
+		return areaObject;
+	}
+
+	//一番古いエリアを削除
+	void DestroyOldestArea()
+	{
+		GameObject oldArea = generatedAreaList [0];
+		generatedAreaList.RemoveAt (0);
+		Destroy (oldArea);
+	}
+}
--- eewano_SampleGame01/Assets/Scripts/PlayerController.cs
+++ eewano_SampleGame01/Assets/Scripts/PlayerController.cs
@@ -2,54 +2,84 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
+	
+	const int MinLane = -3;
+	const int MaxLane = 3;
+	const float LaneWidth = 1.0f;
 
-	//必要なコンポーネントを自動で取得する
 	CharacterController controller;
 	Animator animator;
 
-	public static bool gameOver = false;
+	Vector3 moveDirection = Vector3.zero;
+	int targetLane;
+
+	public float gravity;
+	public float speedZ;
+	public float speedX;
+	public float speedJump;
+	public float accelerationZ;
 
 	void Start()
 	{
+		//必要なコンポーネントを自動で取得する
 		controller = GetComponent<CharacterController> ();
 		animator = GetComponent<Animator>();
-
-		gameOver = false;
-		GetComponent<Collider>().isTrigger = true;
 	}
-
-	public float nowSpeed = 3.0f;
-	public float speedPlus = 0.1f;
 
 	void Update()
 	{
-		if(gameOver)
-		{
-			return;
-		}
+		//デバッグ用
+		if (Input.GetKeyDown ("left"))
+			MoveToLeft ();
+		if (Input.GetKeyDown ("right"))
+			MoveToRight ();
+		if (Input.GetKeyDown ("space"))
+			Jump ();
 
-		nowSpeed += Time.deltaTime * speedPlus;
+		//徐々に加速しZ方向に前進させる
+		float acceleratedZ = moveDirection.z + (accelerationZ * Time.deltaTime);
+		moveDirection.z = Mathf.Clamp (acceleratedZ, 0, speedZ);
 
-		if (Input.GetKey ("left")) {
-			transform.localPosition = new Vector3(
-				transform.localPosition.x - Time.deltaTime * nowSpeed ,
-				transform.localPosition.y ,
-				transform.localPosition.z );
+		//X方向は目標のポジションまでの差分の割合で速度を計算
+		float ratioX = (targetLane * LaneWidth - transform.position.x) / LaneWidth;
+		moveDirection.x = ratioX * speedX;
 
-			animator.SetBool ("Left Run", true);
-		} else {
-			animator.SetBool ("Left Run", false);
-		}
+		//重力分の力を毎フレーム追加
+		moveDirection.y -= gravity * Time.deltaTime;
 
-		if (Input.GetKey ("right")) {
-			transform.localPosition = new Vector3(
-				transform.localPosition.x + Time.deltaTime * nowSpeed ,
-				transform.localPosition.y ,
-				transform.localPosition.z );
-			
-			animator.SetBool ("Right Run", true);
-		} else {
-			animator.SetBool ("Right Run", false);
+		//移動実行
+		Vector3 globalDirection = transform.TransformDirection (moveDirection);
+		controller.Move (globalDirection * Time.deltaTime);
+
+		//移動後接地してたらY方向の速度はリセットする
+		if (controller.isGrounded)
+			moveDirection.y = 0;
+
+		//速度が０以上なら走っているフラグをtrueにする
+		animator.SetBool ("Run", moveDirection.z > 0.0f);
+	}
+
+	//左のレーンに移動を開始
+	public void MoveToLeft()
+	{
+		if (controller.isGrounded && targetLane > MinLane)
+			targetLane--;
+	}
+
+	//右のレーンに移動を開始
+	public void MoveToRight()
+	{
+		if (controller.isGrounded && targetLane < MaxLane)
+			targetLane++;
+	}
+
+	public void Jump()
+	{
+		if (controller.isGrounded) {
+			moveDirection.y = speedJump;
+
+			//ジャンプトリガーを設定
+			animator.SetTrigger ("jump");
 		}
 	}
 }
