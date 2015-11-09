dba10ddf10d3487a69efdf9dc14b033c3b416b4c
YasutakaAwano <eewano@gmail.com>
2015-11-09 12:54:00 +0900

    タイトル画面を作成。

---
+++ eewano_SampleGame01/Assets/Scripts/TitleController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TitleController : MonoBehaviour {
+
+	public void OnStartButtonClicked()
+	{
+		//Stageシーンを呼び出す
+		Application.LoadLevel ("Stage");
+	}
+}
