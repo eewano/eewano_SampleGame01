17838562f6f4fed2e95f1ec69e440a83ff0b7ab3
YasutakaAwano <yasutakaawano@YasutakaAwano-no-MacBook-Pro.local>
2015-11-11 18:11:19 +0900

    1.スコアの設定、及びスコアテキストを表示。 2.移動・ジャンプボタンを画面上に設定。

---
+++ eewano_SampleGame01/Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public PlayerController player;
+	public Text scoreLabel;
+
+	public void Update()
+	{
+		//スコアラベルを更新する
+		int score = CalcScore ();
+		scoreLabel.text = "Score : " + score + "pts";
+	}
+
+	int CalcScore()
+	{
+		//プレイヤーの走行距離をスコアとする
+		return(int)player.transform.position.z;
+	}
+}
