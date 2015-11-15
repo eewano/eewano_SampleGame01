d5de3468f269eac615ced94b594cb266a2394f32
eewano <eewano@gmail.com>
2015-11-15 10:22:31 +0900

    1.タイトル文字やボタンの配置を、Nexus7のディスプレイ用に調整。 2.実機にて動作確認OK。

--- eewano_SampleGame01/Assets/Standard Assets/Utility/EventSystemChecker.cs
+++ eewano_SampleGame01/Assets/Standard Assets/Utility/EventSystemChecker.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System;
 using System.Collections;
 using UnityEngine;
@@ -21,27 +20,3 @@ public class EventSystemChecker : MonoBehaviour
         }
 	}
 }
-=======
-using System;
-using System.Collections;
-using UnityEngine;
-using UnityEngine.EventSystems;
-
-public class EventSystemChecker : MonoBehaviour
-{
-    //public GameObject eventSystem;
-
-	// Use this for initialization
-	void Awake ()
-	{
-	    if(!FindObjectOfType<EventSystem>())
-        {
-           //Instantiate(eventSystem);
-            GameObject obj = new GameObject("EventSystem");
-            obj.AddComponent<EventSystem>();
-            obj.AddComponent<StandaloneInputModule>().forceModuleActive = true;
-            obj.AddComponent<TouchInputModule>();
-        }
-	}
-}
->>>>>>> 1.タイトル文字やボタンの配置を、Nexus7のディスプレイ用に調整。
--- eewano_SampleGame01/Assets/Standard Assets/Utility/ForcedReset.cs
+++ eewano_SampleGame01/Assets/Standard Assets/Utility/ForcedReset.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
@@ -16,22 +15,3 @@ public class ForcedReset : MonoBehaviour
         }
     }
 }
-=======
-using System;
-using UnityEngine;
-using UnityStandardAssets.CrossPlatformInput;
-
-[RequireComponent(typeof (GUITexture))]
-public class ForcedReset : MonoBehaviour
-{
-    private void Update()
-    {
-        // if we have forced a reset ...
-        if (CrossPlatformInputManager.GetButtonDown("ResetObject"))
-        {
-            //... reload the scene
-            Application.LoadLevelAsync(Application.loadedLevelName);
-        }
-    }
-}
->>>>>>> 1.タイトル文字やボタンの配置を、Nexus7のディスプレイ用に調整。
