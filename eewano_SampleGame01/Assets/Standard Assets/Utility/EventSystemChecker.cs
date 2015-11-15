694056b85888b6ea13b40d098f0f799289c792f3
eewano <eewano@gmail.com>
2015-11-15 10:18:56 +0900

    1.タイトル文字やボタンの配置を、Nexus7のディスプレイ用に調整。 2.実機にて動作確認OK。

--- eewano_SampleGame01/Assets/Standard Assets/Utility/EventSystemChecker.cs
+++ eewano_SampleGame01/Assets/Standard Assets/Utility/EventSystemChecker.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 using System;
 using System.Collections;
 using UnityEngine;
@@ -20,3 +21,27 @@ public class EventSystemChecker : MonoBehaviour
         }
 	}
 }
+=======
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EventSystemChecker : MonoBehaviour
+{
+    //public GameObject eventSystem;
+
+	// Use this for initialization
+	void Awake ()
+	{
+	    if(!FindObjectOfType<EventSystem>())
+        {
+           //Instantiate(eventSystem);
+            GameObject obj = new GameObject("EventSystem");
+            obj.AddComponent<EventSystem>();
+            obj.AddComponent<StandaloneInputModule>().forceModuleActive = true;
+            obj.AddComponent<TouchInputModule>();
+        }
+	}
+}
+>>>>>>> 1.タイトル文字やボタンの配置を、Nexus7のディスプレイ用に調整。
