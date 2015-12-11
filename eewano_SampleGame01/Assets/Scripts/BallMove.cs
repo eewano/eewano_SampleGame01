51240ac624ff6de112ba3057075a4a2d8c763195
YasutakaAwano <yasutakaawano@YasutakaAwano-no-MacBook-Pro.local>
2015-12-11 18:50:23 +0900

    1.ステージ02のエリアが完成。

---
+++ eewano_SampleGame01/Assets/Scripts/BallMove.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMove : MonoBehaviour {
+
+	Vector3 startPosition;
+
+	public float amplitude;
+	public float speed;
+
+	void Start()
+	{
+		startPosition = transform.localPosition;
+	}
+
+	void Update ()
+	{
+		//変位を計算する。
+		float x = amplitude * Mathf.Cos(Time.time * speed);
+
+		//xを変位させたポジションに再設定する。
+		transform.localPosition = startPosition + new Vector3(x, 0, 0);
+	}
+}
